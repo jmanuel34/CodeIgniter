fb4f2cb2bb8fb7d5ecff8c1a054b822940c321e0
jmanuel34 <jmanuel34@gmail.com>
2014-01-14 20:56:33 -0600

    Revert 460083a..6e3f15f
    This rolls back to commit 460083afed9161df1b1e6fbd120f47f47d870f2c.

--- application/config/routes.php
+++ application/config/routes.php
@@ -43,7 +43,6 @@
 
 // $route['default_controller'] = 'pages/view';
 // $route['(:any)'] = 'pages/view/$1';
-$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
