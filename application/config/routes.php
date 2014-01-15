e5dab12b6d963fe07789173e3f622f4ab624b132
jmanuel34 <jmanuel34@gmail.com>
2014-01-14 19:37:35 -0600

    BBDD extend
    Acceso a BBDD y creación de items
    http://localhost/index.php/news/create

--- application/config/routes.php
+++ application/config/routes.php
@@ -43,6 +43,7 @@
 
 // $route['default_controller'] = 'pages/view';
 // $route['(:any)'] = 'pages/view/$1';
+$route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
 $route['(:any)'] = 'pages/view/$1';
